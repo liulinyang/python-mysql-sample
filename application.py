29899abcdc388ca7724f168e7e2cc41dde480e01
Liulin Yang <liulinyang@gmail.com>
2015-05-21 10:57:55 +0800

    update a little bit.

--- application.py
+++ application.py
@@ -10,7 +10,7 @@ def hello_world():
   storage = Storage()
   storage.populate()
   score = storage.score()
-  return "Hello world, %d!" % score
+  return "Awesome! %d!" % score
 
 class Storage():
   def __init__(self):
@@ -28,7 +28,7 @@ class Storage():
 
   def populate(self):
     cur = self.db.cursor()
-    cur.execute("INSERT INTO scores(score) VALUES(1234)")
+    cur.execute("INSERT INTO scores(score) VALUES(520)")
 
   def score(self):
     cur = self.db.cursor()
